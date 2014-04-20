a83d13955b92988dd245250b967c49d09faff131
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-04-19 17:01:58 -0700

    added buttons to welcome activity
    added code to launch respective activities

--- src/com/blackdoor/biritracker/WelcomeActivity.java
+++ src/com/blackdoor/biritracker/WelcomeActivity.java
@@ -3,15 +3,21 @@ package com.blackdoor.biritracker;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class WelcomeActivity extends Activity {
+	
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,10 @@ public class WelcomeActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		assignButtons();
+		
+		
 	}
 
 	@Override
@@ -43,12 +53,15 @@ public class WelcomeActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
-
+		
 		public PlaceholderFragment() {
 		}
 
@@ -57,7 +70,21 @@ public class WelcomeActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_welcome,
 					container, false);
+					
 			return rootView;
+		}
+		
+		public void onButtonCreateRide(View v){
+			startNewActivity(getActivity(), CreateActivity.class);
+		}
+		
+		public void onButtonSelectRide(View v){
+			startNewActivity(getActivity(), SelectActivity.class);
+		}
+		
+		private void startNewActivity(Context con, Class dest){
+			Intent i = new Intent(con, dest);
+			startActivity(i);
 		}
 	}
 
