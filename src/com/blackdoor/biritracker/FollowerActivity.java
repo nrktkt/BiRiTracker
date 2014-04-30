4fad4a0e3e146c92461210a0059342b094cecc15
Cj Buresch <cjburesch@gmail.com>
2014-04-29 22:13:07 -0700

    added mapfrag
    map frag might work. gonna test once i get the drivers to see the
    tablet....

--- src/com/blackdoor/biritracker/FollowerActivity.java
+++ src/com/blackdoor/biritracker/FollowerActivity.java
@@ -13,6 +13,8 @@ import pref.prefrences;
 import server.BiRiServer;
 
 import com.blackdoor.biritracker.util.SystemUiHider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -60,150 +62,163 @@ public class FollowerActivity extends Activity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-	
+
 	private Timer updateTimer;
 	private String rideName;
-	//private LatLng lastGoodLoc;
+	// private LatLng lastGoodLoc;
 	private double latitude;
 	private double longitude;
 	private Socket connection;
-	
-	public Handler mMapHandler = new Handler(){
-			public void handleMessage(Message msg){
-				//TODO add code to add a pin for leader at latitude and longitude
-			}
-		};
+	private GoogleMap map;
 
-	private void uiHiderStuff(){
+	public Handler mMapHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			// TODO add code to add a pin for leader at latitude and longitude
+		}
+	};
+
+	private void uiHiderStuff() {
 		final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		final View contentView = findViewById(R.id.fullscreen_content);
 		// Set up an instance of SystemUiHider to control the system UI for
-				// this activity.
-				mSystemUiHider = SystemUiHider.getInstance(this, contentView,
-						HIDER_FLAGS);
-				mSystemUiHider.setup();
-				mSystemUiHider
-						.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-							// Cached values.
-							int mControlsHeight;
-							int mShortAnimTime;
+		// this activity.
+		mSystemUiHider = SystemUiHider.getInstance(this, contentView,
+				HIDER_FLAGS);
+		mSystemUiHider.setup();
+		mSystemUiHider
+				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+					// Cached values.
+					int mControlsHeight;
+					int mShortAnimTime;
 
-							@Override
-							@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-							public void onVisibilityChange(boolean visible) {
-								if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-									// If the ViewPropertyAnimator API is available
-									// (Honeycomb MR2 and later), use it to animate the
-									// in-layout UI controls at the bottom of the
-									// screen.
-									if (mControlsHeight == 0) {
-										mControlsHeight = controlsView.getHeight();
-									}
-									if (mShortAnimTime == 0) {
-										mShortAnimTime = getResources().getInteger(
-												android.R.integer.config_shortAnimTime);
-									}
-									controlsView
-											.animate()
-											.translationY(visible ? 0 : mControlsHeight)
-											.setDuration(mShortAnimTime);
-								} else {
-									// If the ViewPropertyAnimator APIs aren't
-									// available, simply show or hide the in-layout UI
-									// controls.
-									controlsView.setVisibility(visible ? View.VISIBLE
-											: View.GONE);
-								}
-
-								if (visible && AUTO_HIDE) {
-									// Schedule a hide().
-									delayedHide(AUTO_HIDE_DELAY_MILLIS);
-								}
-							}
-						});
-
-				// Set up the user interaction to manually show or hide the system UI.
-				contentView.setOnClickListener(new View.OnClickListener() {
 					@Override
-					public void onClick(View view) {
-						if (TOGGLE_ON_CLICK) {
-							mSystemUiHider.toggle();
+					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+					public void onVisibilityChange(boolean visible) {
+						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+							// If the ViewPropertyAnimator API is available
+							// (Honeycomb MR2 and later), use it to animate the
+							// in-layout UI controls at the bottom of the
+							// screen.
+							if (mControlsHeight == 0) {
+								mControlsHeight = controlsView.getHeight();
+							}
+							if (mShortAnimTime == 0) {
+								mShortAnimTime = getResources().getInteger(
+										android.R.integer.config_shortAnimTime);
+							}
+							controlsView
+									.animate()
+									.translationY(visible ? 0 : mControlsHeight)
+									.setDuration(mShortAnimTime);
 						} else {
-							mSystemUiHider.show();
+							// If the ViewPropertyAnimator APIs aren't
+							// available, simply show or hide the in-layout UI
+							// controls.
+							controlsView.setVisibility(visible ? View.VISIBLE
+									: View.GONE);
+						}
+
+						if (visible && AUTO_HIDE) {
+							// Schedule a hide().
+							delayedHide(AUTO_HIDE_DELAY_MILLIS);
 						}
 					}
 				});
 
-				// Upon interacting with UI controls, delay any scheduled hide()
-				// operations to prevent the jarring behavior of controls going away
-				// while interacting with the UI.
-				findViewById(R.id.buttonLeaveRide).setOnTouchListener(
-						mDelayHideTouchListener);
+		// Set up the user interaction to manually show or hide the system UI.
+		contentView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (TOGGLE_ON_CLICK) {
+					mSystemUiHider.toggle();
+				} else {
+					mSystemUiHider.show();
+				}
+			}
+		});
+
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		findViewById(R.id.buttonLeaveRide).setOnTouchListener(
+				mDelayHideTouchListener);
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_follower);
 		setupActionBar();
-
 		uiHiderStuff();
-		
-		
-		
 		setupTimer(4);
-		
-		
-		
+		setupMap();
 	}
-	
-	private void setupTimer(int x){
+
+	private void setupMap(){
+		setUpMapIfNeeded();
+		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	}
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (map == null) {
+			map = ((MapFragment) getFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (map != null) {
+				// The Map is verified. It is now safe to manipulate the map.
+
+			}
+		}
+	}
+
+	private void setupTimer(int x) {
 		System.out.println("ride created");
 		final int tryout = x;
 		updateTimer = new Timer(true);
-		updateTimer.scheduleAtFixedRate(new TimerTask(){
+		updateTimer.scheduleAtFixedRate(new TimerTask() {
 
 			@Override
 			public void run() {
-				for(int i = 0; i<tryout; i++){
+				for (int i = 0; i < tryout; i++) {
 					try {
 						updateLocationFromServer();
-						i = tryout*2;
+						i = tryout * 2;
 					} catch (IOException e) {
 						e.printStackTrace();
-						Toast toast = Toast.makeText(FollowerActivity.this.getApplicationContext(), 
+						Toast toast = Toast.makeText(
+								FollowerActivity.this.getApplicationContext(),
 								e.getMessage(), Toast.LENGTH_LONG);
 						toast.show();
 					}
 				}
 			}
-			
+
 		}, 0, prefrences.UPDATE_PERIOD);
 	}
-	
-	private void updateLocationFromServer() throws IOException{
+
+	private void updateLocationFromServer() throws IOException {
 		connection = LeaderActivity.connectToServer();
 		PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(connection.getInputStream()));
-		out.println(BiRiServer.Codes.REQUEST.name() + prefrences.NULL +
-				rideName + prefrences.NULL);
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				connection.getInputStream()));
+		out.println(BiRiServer.Codes.REQUEST.name() + prefrences.NULL
+				+ rideName + prefrences.NULL);
+
 		String replyLine = in.readLine();
 		connection.close();
-		
-		StringTokenizer tk = new StringTokenizer(replyLine, ""+prefrences.NULL);
-		if(!tk.nextToken().equalsIgnoreCase(BiRiServer.Codes.LOC.name())){
+
+		StringTokenizer tk = new StringTokenizer(replyLine, ""
+				+ prefrences.NULL);
+		if (!tk.nextToken().equalsIgnoreCase(BiRiServer.Codes.LOC.name())) {
 			throw new IOException("Failed to update location from server");
 		}
 		latitude = Double.parseDouble(tk.nextToken());
 		longitude = Double.parseDouble(tk.nextToken());
-		
+
 	}
 
-	
-	
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
--- src/server/BiRiServer.java
+++ src/server/BiRiServer.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package server;
 
 import java.io.BufferedReader;
@@ -11,29 +8,85 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.concurrent.ConcurrentHashMap;
 
 import pref.prefrences;
 
-/**
- * @author nfischer3
- *
- */
 public class BiRiServer {
-	
+
 	public static final char NULL = '\u0000';
-	public enum Codes{
+
+	public enum Codes {
 		CREATE, // create ride
 		SUBMIT, // submit current location
-		REQUEST, //request current location
+		REQUEST, // request current location
 		DESTROY, // destroy ride
-		RIDELISTREQ,// request list of rides
-		OK,
-		FAIL,
-		LOC, //return last location for a ride
-		RIDELISTREP; //return list of rides
+		RIDELISTREQ, // request list of rides
+		OK, FAIL, LOC, // return last location for a ride
+		RIDELISTREP; // return list of rides
 	}
-	
-	
+
+	private ServerSocket serverSocket;
+	private int port = prefrences.defaultPort;
+	private BiRiDatabase dataBase;
+	private boolean running = true;
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		BiRiDatabase db = new BiRiDatabase();
+		db.createRide("TuNi", "123", "6", "6");
+		System.out.println(db.requestRideList());
+		System.out.println(db.requestLoc("TuNi"));
+		db.submitLoc("123", "7", "7");
+		System.out.println(db.requestLoc("TuNi"));
+		db.createRide("Swamp", "456", "8", "8");
+		db.destroyRide("123");
+		System.out.println(db.requestRideList() + "/");
+
+		/*
+		 * int port; //handle args[]
+		 */
+		BiRiServer server = new BiRiServer(prefrences.defaultPort);
+		server.doServerLoop();
+
+	}
+
+	public BiRiServer(int port) throws IOException {
+		this.port = port;
+		serverSocket = new ServerSocket(port);
+		dataBase = new BiRiDatabase();
+
+	}
+
+	public void start() throws IOException {
+		doServerLoop();
+	}
+
+	public void stop() {
+		running = false;
+	}
+
+	private void doServerLoop() throws IOException {
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(
+				System.in));
+		while (running) {
+			System.out.println("loop");
+
+			try {
+				new ServerThread(serverSocket.accept(), dataBase).start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (keyboard.ready()) {
+				System.out.println("boop");
+			}
+		}
+	}
 }
